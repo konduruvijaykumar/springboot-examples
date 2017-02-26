f0f8da8fea7c983ded6b5807d3291b07d777dabc
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-02-26 23:35:46 +0530

    Vijay - 2017/02/26 - adding spring boot data jpa with rest api

---
+++ security-credentials-api/src/main/java/org/pjay/controller/UserSecurityInfoController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.pjay.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author vijayk
+ *
+ */
+@RestController("/usersecurityinfo")
+public class UserSecurityInfoController {
+
+}

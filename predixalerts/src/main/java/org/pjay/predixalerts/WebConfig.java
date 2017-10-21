72ad3684476c879db435c41dc1b7da22ceeb4f07
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-10-21 13:03:20 +0530

    Vijay - 2017/10/21 - Spring boot examples

---
+++ predixalerts/src/main/java/org/pjay/predixalerts/WebConfig.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.pjay.predixalerts;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author vkonduru3
+ *
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		//super.addCorsMappings(registry);
+		// Allow for complete application 
+		registry.addMapping("/**");
+	}
+
+}
